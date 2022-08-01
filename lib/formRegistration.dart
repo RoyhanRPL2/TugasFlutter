31192072d6eedf0963b36f2fed0c31b1393207fb
Royhan <royhanbumi227@gmail.com>
2022-08-01 11:14:52 +0700

    feat[rb] navigator

--- lib/formRegistration.dart
+++ lib/formRegistration.dart
@@ -20,93 +20,97 @@ class _formRegistrationState extends State<formRegistration> {
       appBar: AppBar(
         title: Text("Form Registration"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextField(
-              controller: ctrUsername,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
-              ),
-            ),
-            Container(
-              height: 10,
-            ),
-            TextField(
-              controller: ctrPassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Pilih Jenis Kelamin",
-              style: TextStyle(fontSize: 15),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 1,
-                  groupValue: id,
-                  onChanged: (val) {
-                    setState(() {
-                      id = 1;
-                    });
-                  },
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                controller: ctrUsername,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
                 ),
-                Text("Male")
-              ],
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 2,
-                  groupValue: id,
-                  onChanged: (val) {
-                    setState(() {
-                      id = 2;
-                    });
-                  },
+              ),
+              Container(
+                height: 10,
+              ),
+              TextField(
+                controller: ctrPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
                 ),
-                Text("Female")
-              ],
-            ),
-            Text("phone :  " + myPhone),
-            ElevatedButton(
-                onPressed: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondRoute()),
-                  );
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Pilih Jenis Kelamin",
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 1,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        id = 1;
+                      });
+                    },
+                  ),
+                  Text("Male")
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 2,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        id = 2;
+                      });
+                    },
+                  ),
+                  Text("Female")
+                ],
+              ),
+              Text("phone :  " + myPhone),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SecondRoute()),
+                    );
 
-                  print('hasil input phone number : ' + result.toString());
-                  setState(() {
-                    myPhone = result.toString();
-                  });
-                  SizedBox(
-                    height: 20,
-                  );
-                },
-                child: Text("phone number")),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-              child: Container(
-                width: 200,
-                child: ElevatedButton(onPressed: () {}, child: Text("Submit")),
+                    print('hasil input phone number : ' + result.toString());
+                    setState(() {
+                      myPhone = result.toString();
+                    });
+                    SizedBox(
+                      height: 20,
+                    );
+                  },
+                  child: Text("phone number")),
+              SizedBox(
+                height: 10,
               ),
-            )
-          ],
+              Center(
+                child: Container(
+                  width: 200,
+                  child:
+                      ElevatedButton(onPressed: () {}, child: Text("Submit")),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
